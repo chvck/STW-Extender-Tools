78cded91394f2ca7b05d5bc1e156b34b8614fdc2
Charles Dixon <chvckd@gmail.com>
2012-02-23 22:55:21 +0000

    Fixed bug related to users being empty causing an exception

--- js/add_tools.js
+++ js/add_tools.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function ($) {
     var users
-        , showText = "<p class='showLink' style='cursor:pointer; color:#3F6895; text-decoration:underline;'>Show this post</p>"; 
-    
-    function hidePosts(showSomeonePosted) {
+        , showText = "<p class='showLink' style='cursor:pointer; color:#3F6895; text-decoration:underline;'>Show this post</p>"
+        ,showSomeonePosted;
+        
+    function hidePosts() {
         $('.threadauthor strong').each(function () {
             var $parent = $(this).parent().parent()
                 , $post = $parent.children('.post');
@@ -16,11 +17,33 @@ jQuery(document).ready(function ($) {
                 }
             }
         });
+            
+        $('.showLink').live('click', function () {
+            $(this).parent().children().show();
+            $(this).hide();
+        });
+
+        $('.threadauthor small').append(" - <span class='hideUser' style='display:inline; cursor:pointer; color:#3F6895; text-decoration:underline;'>Block User</span>");
+     
+        $('.hideUser').live('click', function () {
+            var user = $(this).parent().siblings('strong').text();
+            if (users.indexOf(user) === -1) {
+                chrome.extension.sendRequest({msg: 'hide_user', username: user}, function (response) {
+                    users = response.result.users;              
+                });
+                var $parent = $(this).parent().parent().parent()
+                    , $post = $parent.children('.post');
+                if (showSomeonePosted) {
+                    $parent.hide();
+                    $parent.after(showText);
+                } else {
+                    $post.hide();
+                }
+            } 
+        });  
     }
-        
+   
     function hideThreads() {
-        var users = localStorage.blockedUsers.split(',');
-        
         $('td.num').each(function () {
             if (users.indexOf($(this).text()) !== -1) {
                 $(this).parent().hide();
@@ -28,32 +51,33 @@ jQuery(document).ready(function ($) {
         });
     }
 
-	$('.threadauthor small').append(" - <p  class='hideUser' style='display:inline; cursor:pointer; color:#3F6895; text-decoration:underline;'>Block User</p>");    
-    
-    $('.showLink').live('click', function () {
-		$(this).parent().children().show();
-		$(this).hide();
-	});
-    
-    $('.hideUser').click(function () {
-		var user = $(this).parent().siblings('strong').text();
-		if (users.indexOf(user) === -1) {
-			chrome.extension.sendRequest({msg: 'hide_user', username: user}, function (response) {
-			});
-			var parent = $(this).parent().parent().parent();
-			var post = parent.children('.post');
-			post.hide();
-			post.after(content);
-		} 
-	});
+    function addEasyQuotes() {
+        $('.threadauthor small').append(" - <span class='easyQuote' style='display:inline; cursor:pointer; color:#3F6895; text-decoration:underline;'>Quote</span>");
+            
+        $('.easyQuote').live('click', function () {
+            var $parent = $(this).parent().parent().parent()
+                , $post = $parent.children('.post')
+                , quote;        
+
+            quote = '[quote]' + $post.text() + '[/quote]';
+            $('#post_content').val(quote);
+            
+            document.getElementById('postform').scrollIntoView();
+        });  
+    }
+
     
     chrome.extension.sendRequest({msg: 'get_options'}, function (response) {
         users = response.result.users;
-        if (response.result.enableHideUsers) {
-            hidePosts(response.result.showSomeonePosted);
+        showSomeonePosted = response.result.showSomeonePosted;
+        if (response.result.enableHideUsers && users) {
+            hidePosts();
         }
-        if (response.result.enableHideThreads) {
-            //hideThreads();
+        if (response.result.enableHideThreads && users) {
+            hideThreads();
+        }
+        if (response.result.enableEasyQuoting) {
+            addEasyQuotes();
         }
     });
 });
